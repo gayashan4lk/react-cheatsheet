ab1df9603cff9b853eae24e824a98346b70e8926
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-15 19:18:02 +0530

    state and lifecycle

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,6 +1,31 @@
 import { React, Component } from 'react';
 
 class Clock extends Component {
+  constructor(props) {
+    console.log("constructor() called.");
+    super(props);
+    this.state.datetime = {time : new Date()};
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount() called.");
+    this.timerID = setInterval(
+      () => this.tick(), 1000
+    );
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount() called.");
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    console.log("tick() called.");
+    this.setState({
+      datetime : {time : new Date()}
+    });
+  }
+
   state = {
     data : "here is some data"
   };
@@ -8,9 +33,8 @@ class Clock extends Component {
     return (
       <div>
         <h1>Hello World!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}</h2>
+        <h2>It is {this.state.datetime.time.toLocaleTimeString()}</h2>
         <h2>Data from State : {this.state.data} </h2>
-        {console.log(this.state)}
       </div>
     );
   }
